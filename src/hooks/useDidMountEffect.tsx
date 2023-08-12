7b24e9aadce72b903449b3aa4f32e7e6af28a1c1
Gon <116235243+Gon1782@users.noreply.github.com>
2023-08-12 19:46:19 +0900

    Update useDidMountEffect.tsx

--- src/hooks/useDidMountEffect.tsx
+++ src/hooks/useDidMountEffect.tsx
@@ -3,8 +3,6 @@ import { useEffect, useRef } from 'react';
 const useDidMountEffect = (func: () => any, deps: Array<any>) => {
   const didMount = useRef(false);
 
-  console.log(didMount);
-
   useEffect(() => {
     if (didMount.current) func();
     else didMount.current = true;
